5e04c00c36e8539bbac99c2ae74697881235b9d9
JDarfix Ssj <jalfsan3108@g.educaand.es>
2026-04-24 19:25:41 +0200

    Add files via upload

---
+++ Problema 1/app/Http/Controllers/Controllers/Controller.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Controlador base del que heredan todos los demás controladores.
+ *
+ * @author  Alumno DWES
+ * @version 1.0
+ */
+
+namespace App\Http\Controllers;
+
+abstract class Controller
+{
+    //
+}
